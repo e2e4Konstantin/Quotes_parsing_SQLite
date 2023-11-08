c823c4a384d675921aa7b4e72d46419a1dc00512
Konstantin Kazak <e2e4suchok@gmail.com>
2023-11-08 20:59:40 +0300

    test

--- TryOut/duckDB/read_excel_data.py
+++ TryOut/duckDB/read_excel_data.py
@@ -2,32 +2,44 @@
 
 import duckdb
 from icecream import ic
+import pandas as pd
 
-con = duckdb.connect()
+con = duckdb.connect("statistics.duckdb")
 con.install_extension("spatial")
 con.load_extension("spatial")
 
 # con.execute("INSTALL spatial;")
 # con.execute("LOAD spatial;")
+#
+# result = con.execute("CREATE TABLE new_tbl AS SELECT * FROM st_read('statistics.xlsx', layer='Export Worksheet', open_options=ARRAY[OGR_XLSX_HEADERS=FORCE, OGR_XLSX_FIELD_TYPES = STRING]);")
+# ic(result.execute('select count(*) from new_tbl').fetchall())
+#
+# result = con.execute("""
+#     INSERT INTO new_tbl SELECT * FROM st_read('statistics.xlsx', layer='Sheet1', open_options=ARRAY[OGR_XLSX_HEADERS=DISABLE, OGR_XLSX_FIELD_TYPES = STRING]);
+#     """)
 
-result = con.execute("CREATE TABLE new_tbl AS SELECT * FROM st_read('statistics.xlsx', layer='Export Worksheet');")
-ic(result)
+print(con.execute("SHOW ALL TABLES;").fetchall())
+print(con.execute("DESCRIBE new_tbl;").fetchall())
+print(con.table('new_tbl').show())
+
+
+
+
+
+rel = con.sql('SELECT ANY_VALUE(PRESSMARK), sum(VOLUME) AS vol_sum FROM new_tbl')
+
+vol_sum_column = rel["vol_sum"]
+print(vol_sum_column)
+
+
+
 # ic(result.show())
-ic(result.execute('select count(*) from new_tbl').fetchall())
+ic(con.execute('select count(*) from new_tbl').fetchall())
 
+df = con.execute('select * from new_tbl').df()
+print(df)
 
-
-# print(result.execute('select * from new_tbl').fetchall())  #count(*)
+print(con.execute('select * from new_tbl LIMIT 5').fetchall())
 # result.close()
-# con.close()
+con.close()
 
-# with duckdb.connect('file.db') as con:
-#     con.sql('CREATE TABLE test(i INTEGER)')
-#     con.sql('INSERT INTO test VALUES (42)')
-#     con.table('test').show()
-
-
-
-
-# INSERT INTO tbl
-# SELECT * FROM st_read('test_excel.xlsx', layer='Sheet1');
---
+++ TryOut/statistics/excel_read_to_DB_2.py
@@ -0,0 +1,44 @@
+import sqlite3
+import openpyxl
+from openpyxl import load_workbook
+from icecream import ic
+
+# excel_file_name = r"C:\Users\kazak.ke\Documents\Задачи\Парсинг_параметризация\SRC\Статистика_20.xlsx"
+# excel_file_name = r"F:\Kazak\GoogleDrive\1_KK\Job_CNAC\АИС\Статистика_21.xlsx"
+excel_file_name = r"statistics.xlsx"
+
+# PRESSMARK	TITLE	UOM	VOLUME	CARD_NUM	OBJ_NAME	ACTIVITY_TYPE	OBJ_TYPE	OBJ_GROUP	EXAMINATION_DATE
+
+
+con = sqlite3.connect('statistics_raw.sqlite')
+create_query = """CREATE TABLE IF NOT EXISTS tblRawStatistics ( CASE_NUM TEXT, PRESSMARK TEXT, TITLE TEXT, UOM TEXT, VOLUME TEXT, CARD_NUM TEXT, OBJ_NAME TEXT, ACTIVITY_TYPE TEXT, OBJ_TYPE TEXT, OBJ_GROUP TEXT, EXAMINATION_DATE TEXT
+);"""
+insert_query = """
+    INSERT INTO tblRawStatistics 
+        (
+        CASE_NUM, PRESSMARK, TITLE, UOM, VOLUME, CARD_NUM, OBJ_NAME, ACTIVITY_TYPE, OBJ_TYPE, OBJ_GROUP, EXAMINATION_DATE)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
+"""
+con.execute(create_query)
+
+d1 = ('МГЭ/34877-1/5', '0.0-0-0', 'МАССА МУСОРА', 'т', '22.08', '77-10171/20-(0)-0', 'Капитальный ремонт многоквартирного дома. г. Москва, улица Строителей, д. 4, к. 4 (ЮЗАО, Гагаринский)', 'Капитальный ремонт', 'Производственного и непроизводственного назначения', 'Многоэтажный многоквартирный жилой дом', '2021-01-13 00:00:00')
+
+wb = load_workbook(filename=excel_file_name, data_only=True)
+sheets = wb.sheetnames
+ic(sheets)
+
+for sheet in sheets:
+    ws = wb[sheet]
+    ic(ws.title)
+    ic(type(ws.rows))
+    x = next(ws.rows)
+
+    for row in ws.iter_rows(min_row=2):
+        column_max = 10
+        data = tuple([str(row[i].value) for i in range(column_max+1)])
+        print(data)
+        con.execute(insert_query, data)
+
+i= con.execute("""PRAGMA table_info(tblRawStatistics);""")
+print(i.fetchall())
+
